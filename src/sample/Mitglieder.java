d7578f08e855013680c0c3a8a180012a24366bb2
LoCaMaurer <42177826+LoCaMaurer@users.noreply.github.com>
2019-10-27 16:13:50 +0100

    Add files via upload

---
+++ src/sample/Mitglieder.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Mitglieder {
+
+    ArrayList<String> projektMitglieder;
+
+    {
+        projektMitglieder = new ArrayList<>();
+    }
+}
